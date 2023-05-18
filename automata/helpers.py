cc9eff94b7d49fc367c7569cc0f97092f36026a9
enzo <enzo.bonansea@gmail.com>
2023-05-18 00:50:38 -0300

    fix: afnd->afd

--- automata/helpers.py
+++ automata/helpers.py
@@ -6,9 +6,9 @@ def get_union_automata(automata1: AFND, automata2: AFND) -> AFND:
     automata2.normalize_states("b")
 
     automata = AFND()
-    automata.states = automata1.states.union(automata2.states)
-    automata.transitions = automata1.transitions | automata2.transitions
-    automata.alphabet = automata1.alphabet.union(automata2.alphabet)
+    automata.states = set(automata1.states | automata2.states)
+    automata.transitions = dict(automata1.transitions | automata2.transitions)
+    automata.alphabet = set(automata1.alphabet | automata2.alphabet)
 
     automata.add_state(0)
     automata.mark_initial_state(0)
@@ -49,29 +49,17 @@ def get_concat_automata(automata1: AFND, automata2: AFND) -> AFND:
 
 
 def get_star_automata(aut: AFND) -> AFND:
-    aut.normalize_states()
-    aut.add_state("f", final=True)
-    aut.add_state("i")
-    aut.mark_initial_state("i")
-    aut.add_transition("i", aut.initial_state, SpecialSymbol.Lambda)
-    aut.add_transition("i", "f", SpecialSymbol.Lambda)
     for i in aut.final_states:
-        aut.add_transition(i, "f", SpecialSymbol.Lambda)
         aut.add_transition(i, aut.initial_state, SpecialSymbol.Lambda)
-
+        aut.add_transition(aut.initial_state, i, SpecialSymbol.Lambda)
     aut.normalize_states()
+
     return aut
 
 
 def get_plus_automata(aut: AFND) -> AFND:
-    aut.normalize_states()
-    aut.add_state("f", final=True)
-    aut.add_state("i")
-    aut.mark_initial_state("i")
-    aut.add_transition("i", aut.initial_state, SpecialSymbol.Lambda)
     for i in aut.final_states:
-        aut.add_transition(i, "f", SpecialSymbol.Lambda)
         aut.add_transition(i, aut.initial_state, SpecialSymbol.Lambda)
-
     aut.normalize_states()
+    
     return aut
